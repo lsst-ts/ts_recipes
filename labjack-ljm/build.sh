90562e9d832aac1aeae03afd8cca289b16312709
Russell Owen <rowen@uw.edu>
2021-12-21 17:10:27 -0800

    Add labjack-ljm recipe

---
+++ labjack-ljm/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Version string in the C library installer's file name
+file_version=2019_07_16
+# The desired architecture, as descripbed in the C library installer's file name
+arch=x86_64
+# The version of the C library in the above file.
+# To find this information:
+# * Download and expand the installer.
+# * cd into the new directory.
+# * Run labjack_ljm_installer.run (a self-extracting archive) to expand the library code:
+#   ./labjack_ljm_installer.run --keep --noexec
+# Look in labjack_ljm_software for the library.
+c_version=1.20.1
+# Python wrapper version
+python_version=1.21.0
+
+echo "Download and unpack labjack-ljm C library labjack_ljm_software_${file_version}_${arch}.tar.gz"
+wget https://labjack.com/sites/default/files/software/labjack_ljm_software_${file_version}_${arch}.tar.gz
+# labjack_ljm_installer.run creates a directory labjack_ljm_software
+# which includes setup.sh, the script that installs the library.
+# It hard-codes the destination, so run sed to patch it.
+tar -xzf labjack_ljm_software_${file_version}_${arch}.tar.gz
+cd labjack_ljm_software_${file_version}_${arch}
+./labjack_ljm_installer.run --keep --noexec
+
+echo "Install labjack-ljm C library ${c_version}"
+cd labjack_ljm_software
+# The installer file has /usr/local hard-coded; patch it:
+sed "s|=/usr/local|=${PREFIX}|" setup.sh >temp.sh
+# Stop before doing the following steps:
+# * ldconfig, because it tries to update system ldconfig, which is the wrong one
+# * Installing rules: setup.sh puts them in /etc; where is that in conda images?
+#   I hope we don't need them.
+# * Installing Kipling: we don't need it. Note that there is also
+#   an undocumented command-line argument for this, in case we figure out
+#   how to update the right ldconfig and install rules in the correct location.
+sed "s|install_ljm_constants$|install_ljm_constants\n\n# Quit early\nsuccess|" temp.sh >patchedsetup.sh
+rm temp.sh
+# Show the diff; `|| true` makes this command return 0, so the script continues.
+echo "Diff between patched and unpatched install script:"
+diff patchedsetup.sh setup.sh || true
+bash patchedsetup.sh ${c_version}
+
+# This command is based on the conda's click example
+echo "Install labjack-ljm Python wrapper ${python_version}"
+python -m pip install labjack-ljm==${python_version} --no-deps --ignore-installed -vv
