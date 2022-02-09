ccdd6c3d372ec27e84106e735c915717a334861f
Dave Mills <rfactory@theriver.com>
2022-02-09 11:56:31 -0700

    Make OpenSplice version more flexible

--- ts_opensplice_community/build.sh
+++ ts_opensplice_community/build.sh
@@ -4,7 +4,7 @@ cp release.com ${PREFIX}/bin/opensplice_activate
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 cp "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
 
-pip install tools/python/site-packages/dds-6.9*.whl
+pip install tools/python/site-packages/dds-6.*.whl
 cp -r bin  docs  etc  include  jar  lib  LICENSE  README.md  release.com  tools ${PREFIX}/.
 
 
--- ts_opensplice_private/build.sh
+++ ts_opensplice_private/build.sh
@@ -4,7 +4,7 @@ cp release.com ${PREFIX}/bin/opensplice_activate
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 cp "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
 
-pip install tools/python/site-packages/dds-6.10*.whl
+pip install tools/python/site-packages/dds-6.*.whl
 cp -r bin etc include jar lib LICENSE README.md release.com  tools ${PREFIX}/.
 
 
